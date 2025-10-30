c468b04a9c7b7fd491e59ab781c5ac6da33ea75a
Sasterr1 <farhanardianur@gmail.com>
2025-10-30 09:44:02 +0800

    KUIS

---
+++ kuis.js
@@ -0,0 +1,33 @@
+class Employee {
+    constructor(name, salary) {
+        this.name = name;
+        this.baseSalary = salary; 
+    }
+
+    calculateBonus() {
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    calculateBonus() {
+        let bonusSalary = this.baseSalary * 0.10;
+        console.log(`Bonus ${this.name} (Karyawan Tetap, dengan gaji tetap Rp${this.baseSalary}): ${this.baseSalary} x 10% = Rp${bonusSalary}`);
+        return bonusSalary;
+    }
+}
+
+class ContractEmployee extends Employee {
+    calculateBonus() {
+        let bonusSalary = this.baseSalary * 0.05;
+        console.log(`Bonus ${this.name} (Karyawan Kontrak, dengan gaji kontrak Rp${this.baseSalary}): ${this.baseSalary} x 5% = Rp${bonusSalary}`);
+        return bonusSalary;
+    }
+}
+
+const GajiFullTime = new FullTimeEmployee("Budi", 8000000);
+const GajiContract = new ContractEmployee("Sita", 6000000);
+
+let totalBonus = GajiFullTime.calculateBonus() + GajiContract.calculateBonus();
+
+console.log("---------------------------------");
+console.log(`Total Bonus Seluruh Karyawan: Rp${totalBonus}`);
